4f4949df8c048dfcb7d95460141a0ecef0107626
mat ess <mat@mat.services>
2022-08-13 19:50:20 -0400

    Fix nix build

--- flake.nix
+++ flake.nix
@@ -21,13 +21,13 @@
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "personal-site";
-          version = "2022-08-07";
+          version = "2022-08-13";
           src = ./.;
           nativeBuildInputs = with pkgs; [ scour zola ];
           configurePhase = ''
             mkdir -p "themes/${themeName}"
             cp -r ${theme}/* "themes/${themeName}"
-            scour -i static/favicon-unoptimized.svg -o static/favicon.svg
+            scour -i static/image/_favicon-original.svg -o static/image/favicon.svg
           '';
           buildPhase = "zola build";
           installPhase = "cp -r public $out";
