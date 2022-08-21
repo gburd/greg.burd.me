93463b54e0bce01feae89140076399b30ab2ba3b
mat ess <mat@mat.services>
2022-08-21 17:46:49 -0400

    Better image optimizing

--- flake.nix
+++ flake.nix
@@ -18,30 +18,37 @@
     flake-parts.lib.mkFlake { inherit self; } {
       imports = [ ];
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          optimize-images = pkgs.writeShellScriptBin "optimize-images" ''
+            shopt -s globstar nullglob
+            ${pkgs.scour}/bin/scour -i static/image/_favicon.svg -o static/image/favicon.svg
+            for image in content/**/_*.png static/image/**/_*.png; do
+              path=$(dirname $image)
+              file=$(basename $image)
+              ${pkgs.pngquant}/bin/pngquant --quality 90-99 -f -o "$path/''${file:1}" $image
+            done
+          '';
+        in
+        {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "personal-site";
           version = "2022-08-13";
           src = ./.;
-          nativeBuildInputs = with pkgs; [ pngquant scour zola ];
+            nativeBuildInputs = with pkgs; [ optimize-images zola ];
           configurePhase = ''
             mkdir -p "themes/${themeName}"
             cp -r ${theme}/* "themes/${themeName}"
-            scour -i static/image/_favicon.svg -o static/image/favicon.svg
-            for image in content/**/_*.png static/image/**.png; do
-              path=$(dirname $image)
-              file=$(basename $image)
-              pngquant --quality 90-99 -f -o "$path/''${file:1}"
-            done
+              optimize-images
           '';
           buildPhase = "zola build";
           installPhase = "cp -r public $out";
         };
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ zola ];
+          devShells.default = with pkgs; mkShell {
+            packages = [ optimize-images flyctl zola ];
           shellHook = ''
             mkdir -p themes
-            ln -sn "${theme}" "themes/${themeName}"
+              ln -snf "${theme}" "themes/${themeName}"
           '';
         };
       };
