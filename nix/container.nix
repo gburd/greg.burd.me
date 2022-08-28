d74eb1df58a0deb906d959b7522d588b8cf655e7
mat ess <mat@mat.services>
2022-08-28 14:09:40 -0400

    Remove "net" logger

--- nix/container.nix
+++ nix/container.nix
@@ -8,7 +8,6 @@ dockerTools.buildLayeredImage {
     Cmd = [ "${caddy}/bin/caddy" "run" "-config" "${caddyfile}" ];
     Env = [
       "SITE_ROOT=${site}"
-      "LOG_OUTPUT_ADDR=tcp://[fdaa:0:742b:a7b:ab2:1:a2b5:2]:9090"
     ];
   };
 }
