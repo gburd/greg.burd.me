7fa6f64a2a6764f0d70d67d4cb5a693dd13caa0b
mat ess <mat@mat.services>
2022-08-28 02:41:20 -0400

    Add privacy, rework some css

--- nix/container.nix
+++ nix/container.nix
@@ -8,6 +8,7 @@ dockerTools.buildLayeredImage {
     Cmd = [ "${caddy}/bin/caddy" "run" "-config" "${caddyfile}" ];
     Env = [
       "SITE_ROOT=${site}"
+      "LOG_OUTPUT_ADDR=tcp://[fdaa:0:742b:a7b:ab2:1:a2b5:2]:9090"
     ];
   };
 }
