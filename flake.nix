36214b9ba6e7b1ddb333f31d96f6c3198b86cb23
mat ess <mat@mat.services>
2022-10-20 19:48:14 -0400

    Use drone env var

--- flake.nix
+++ flake.nix
@@ -18,31 +18,29 @@
           inherit (gitignore.lib) gitignoreSource;
           inherit (pkgs.callPackage ./nix { }) fonts optimize-images;
           inherit (fonts) copyFonts linkFonts;
+          buildSite = { prod }:
+            let inherit (pkgs.lib) optionalString; in ''
+              optimize-images
+              ${optionalString (!prod) "BASE_URL=https://$DRONE_COMMIT_SHA--mat-services.netlify.app"}
+              zola build --drafts ${optionalString (!prod) "--base-url $BASE_URL"}
+              # zola's ignored_content setting doesn't work in static/
+              rm -rf public/image/_favicon.svg
+            '';
         in
         {
           packages.default = with pkgs; stdenv.mkDerivation {
             pname = "personal-site";
             version = "2022-10-10";
             src = gitignoreSource ./.;
-            nativeBuildInputs = [ optimize-images zola ];
+            nativeBuildInputs = [ git optimize-images zola ];
             configurePhase = copyFonts;
-            buildPhase = ''
-              optimize-images
-              zola build --drafts
-              # zola's ignored_content setting doesn't work in static/
-              rm -rf public/image/_favicon.svg
-            '';
+            buildPhase = buildSite { prod = true; };
             installPhase = ''
               cp -r public $out
             '';
           };
           packages.staging-site = config.packages.default.overrideAttrs (_: {
-            buildPhase = ''
-              optimize-images
-              zola build --drafts --base_url $DEPLOY_PRIME_URL
-              # zola's ignored_content setting doesn't work in static/
-              rm -rf public/image/_favicon.svg
-            '';
+            buildPhase = buildSite { prod = false; };
           });
           devShells.default = with pkgs; mkShell {
             packages = [ optimize-images zola ];
